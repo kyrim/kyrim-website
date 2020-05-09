3e246749e77f3b67177bca1d644003e85ada62e7
kyrimsteele <kyrimcsteele@gmail.com>
2020-05-09 18:41:41 +1000

    Fix my projects layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ const NoExperienceMessage = styled.span`
 
 const ProjectContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
   gap: 2rem;
 `;
 
